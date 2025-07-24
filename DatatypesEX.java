ce2ac762d1394626700830766f75cbefa1327320
ctamilarasan <tamilarasant556@gmail.com>
2025-07-24 15:58:12 +0530

    Add files via upload
    day2

---
+++ DatatypesEX.java
@@ -0,0 +1,25 @@
+package day2;
+
+public class DatatypesEX {
+	public static void main(String[] args) {
+		int a= 5;
+		float b =5.5f;
+		boolean c= true;
+		byte d=126;
+		short e=14;
+		long f= 1234567890l;
+		double g=414.1234;
+		char h='T';
+		
+		System.out.println("the intger: "+a);
+		System.out.println("the float: "+b);
+		System.out.println("the boolean : "+c);
+		System.out.println("the byte : "+d);
+		System.out.println("the short: "+e);
+		System.out.println("the long: "+f);
+		System.out.println("the double: "+g);
+		System.out.println("the char: "+h);
+		
+	}
+
+}
