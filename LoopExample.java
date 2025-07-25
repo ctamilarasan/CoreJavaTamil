a2c1e43143d8e3e87cf0a064c35d84aa9f7c7d8b
ctamilarasan <tamilarasant556@gmail.com>
2025-07-25 14:28:17 +0530

    day7

---
+++ LoopExample.java
@@ -0,0 +1,19 @@
+package day7;
+
+public class LoopExample {
+	public static void main(String[] args) {
+		System.out.println("Normal Order");
+		
+		System.out.println("Normal Order");
+		for(int i=1;i<=50;i++) {
+			System.out.println(i);
+		}
+		System.out.println();
+		System.out.println("Reverse Order");
+		for(int i=50;i>=1;i--) {
+			System.out.println(i);
+		}
+		
+	}
+
+}
