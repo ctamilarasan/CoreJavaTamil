eb6a1e03a61e1cab4f1eeb6d54103bddace35670
ctamilarasan <tamilarasant556@gmail.com>
2025-08-29 13:14:23 +0530

    oops_poly

---
+++ dog_overrinding.java
@@ -0,0 +1,10 @@
+package Opps_polymorphismEX;
+
+public class dog_overrinding extends Animals_overridingEX {
+	@Override
+	void sound() 
+	{
+		System.out.println("dog make sound: boew boew ");
+	}
+
+}
