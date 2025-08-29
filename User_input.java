b3441958808116b5717d2235e8d2c27d18688651
ctamilarasan <tamilarasant556@gmail.com>
2025-08-29 12:55:00 +0530

    ScannerEX2

---
+++ User_input.java
@@ -0,0 +1,16 @@
+package ScannerEX;
+import java.util.Scanner;
+
+
+public class User_input {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter any number: ");
+		 int c=sc.nextInt();
+		 
+		 System.out.println("number from user input = "+c);
+		
+		
+	}
+
+}
