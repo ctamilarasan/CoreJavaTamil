30eb7ee9114494110a2bd5cf505ef5246dddf299
ctamilarasan <tamilarasant556@gmail.com>
2025-08-29 13:13:56 +0530

    oops_poly

---
+++ cat_overriding.java
@@ -0,0 +1,10 @@
+package Opps_polymorphismEX;
+
+public class cat_overriding  extends Animals_overridingEX{
+	@Override
+	void sound() 
+	{
+		System.out.println("cat makes sound : mewo mewo ");
+	}
+
+}
