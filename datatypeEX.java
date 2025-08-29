140d18aecb4b5c07477652a423d5a23b5ded8f4b
ctamilarasan <tamilarasant556@gmail.com>
2025-08-29 12:52:20 +0530

    datatypes

---
+++ datatypeEX.java
@@ -0,0 +1,25 @@
+package datatypesEX;
+
+public class datatypeEX {
+	public static void main(String[] args) {
+		
+		int a=5;
+		float b=5.5f;
+		int num=3;
+		boolean u=(num>8);
+		byte d=126;
+		short e=14;
+		long f=3263534434l;
+		double g=454.9879;
+		char h='a';
+		System.out.println("the integer: "+a);
+		System.out.println("the folat : "+b);
+		System.out.println("the boolean: "+u);
+		System.out.println("the byte: "+d);
+		System.out.println("the short : "+e);
+		System.out.println("the long : "+f);
+		System.out.println("the double : "+g);
+		System.out.println("the char: "+h);
+	}
+
+}
