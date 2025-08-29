cbd18531291ecc0f6efda4ea29205f6117baa9e5
ctamilarasan <tamilarasant556@gmail.com>
2025-08-29 12:57:38 +0530

    loop_whileloop

---
+++ whileLoopEX.java
@@ -0,0 +1,14 @@
+package LoopEx;
+
+public class whileLoopEX {
+	public static void main(String[] args) {
+		
+		int i=1;
+		while(i<=5) 
+		{
+			System.out.println("hi "+i);
+			i++;
+		}
+	}
+
+}
